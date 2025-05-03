50d6342c1550ad0e42f8715b4e4e39a4dac9e78d
Real-Beginner <146841402+Real-Beginner@users.noreply.github.com>
2025-05-03 15:42:49 +0100

    Update and rename HexGrid.java to HexOust.java

--- HexOust.java
+++ HexOust.java
@@ -4,7 +4,7 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class HexGrid extends Application {
+public class HexOust extends Application {
 
     public static void main(String[] args) {
         launch(args);
